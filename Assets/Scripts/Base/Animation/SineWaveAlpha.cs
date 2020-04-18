09b520bd0428e1ea823587048b4eb3cdeff8403c
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2020-04-18 00:48:49 -0700

    - control highlight sprites in scene

---
+++ Assets/Scripts/Base/Animation/SineWaveAlpha.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineWaveAlpha : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = default;
+
+    [SerializeField]
+    private float minAlpha = default;
+
+    [SerializeField]
+    private float maxAlpha = default;
+
+    [SerializeField]
+    private float offsetTime = default;
+
+    [SerializeField]
+    private bool snapAlpha = false;
+
+    [SerializeField]
+    private float snapIncrement = 0f;
+
+    void Update()
+    {
+        float sine = (Mathf.Sin((Time.time * speed) + offsetTime) * 0.5f) + 0.5f;
+        float alpha = minAlpha + ((maxAlpha - minAlpha) * sine);
+
+        if (snapAlpha)
+        {
+            alpha = Mathf.Round(alpha / snapIncrement) * snapIncrement;
+        }
+
+        Color newColor = GetComponent<Renderer>().material.color;
+        newColor.a = alpha;
+        GetComponent<Renderer>().material.color = newColor;
+    }
+}
---
+++ Assets/Scripts/Effects/LightsManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<SpriteRenderer> lights = default;
+
+    [Header("Fade Params")]
+    [SerializeField]
+    private float speed = default;
+
+    [SerializeField]
+    private float minAlpha = default;
+
+    [SerializeField]
+    private float maxAlpha = default;
+
+    [SerializeField]
+    private float offsetTime = default;
+
+    [SerializeField]
+    private bool snapAlpha = false;
+
+    [SerializeField]
+    private float snapIncrement = 0f;
+
+    private void Update()
+    {
+        float sine = (Mathf.Sin((Time.time * speed) + offsetTime) * 0.5f) + 0.5f;
+        float alpha = minAlpha + ((maxAlpha - minAlpha) * sine);
+
+        if (snapAlpha)
+        {
+            alpha = Mathf.Round(alpha / snapIncrement) * snapIncrement;
+        }
+
+        foreach (SpriteRenderer light in lights)
+        {
+            Color newColor = light.color;
+            newColor.a = alpha;
+            light.color = newColor;
+        }
+    }
+}
