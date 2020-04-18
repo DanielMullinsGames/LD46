db915346c04edbd3fc82921f22589e06eb8d8b8b
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2020-04-18 00:37:00 -0700

    - animated wheels on train

--- Assets/Scripts/Base/Animation/FixedRotation.cs
+++ Assets/Scripts/Base/Animation/FixedRotation.cs
@@ -6,7 +6,7 @@ public class FixedRotation : MonoBehaviour
 {
     public float rotation;
 
-	void Update ()
+	void LateUpdate ()
     {
         transform.rotation = Quaternion.Euler(0f, 0f, rotation);
 	}
