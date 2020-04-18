344c6d8c72b97f822e97cff4f32c44ad8aab2f31
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2020-04-18 12:23:48 -0700

    - display heartrate and basic system for heart vitality

---
+++ Assets/Scripts/GameSystems/Machines/HeartMachine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartMachine : Singleton<HeartMachine>
+{
+    public float Vitality { get; private set; }
+
+    [SerializeField]
+    private float vitalityDecayRate;
+
+    private void Start()
+    {
+        Vitality = 1f;
+    }
+
+    void Update()
+    {
+        Vitality = Mathf.Clamp(Vitality - (vitalityDecayRate * Time.deltaTime), 0f, 1f);
+
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Boost();
+        }
+#endif
+    }
+
+    public void Boost()
+    {
+        Vitality = Mathf.Clamp(Vitality + 0.1f, 0f, 1f);
+    }
+}
---
+++ Assets/Scripts/UI/HeartrateDisplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartrateDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private float scrollSpeed;
+
+    [SerializeField]
+    private float pieceWidth;
+
+    [SerializeField]
+    private List<Transform> pieces;
+
+    [SerializeField]
+    private Transform leftMarker;
+
+    [SerializeField]
+    private Transform rightMarker;
+
+    [SerializeField]
+    private List<Sprite> rateSprites;
+
+    private void Update()
+    {
+        foreach (Transform piece in pieces)
+        {
+            float x = piece.localPosition.x + (Time.deltaTime * scrollSpeed);
+            piece.localPosition = new Vector2(x, piece.localPosition.y);
+            if (piece.localPosition.x > rightMarker.localPosition.x + (pieceWidth / 2f))
+            {
+                piece.localPosition = new Vector2(leftMarker.localPosition.x - (pieceWidth / 2f), piece.localPosition.y);
+                SetRateSprite(piece);
+            }
+        }
+    }
+
+    private void SetRateSprite(Transform piece)
+    {
+        piece.GetComponent<SpriteRenderer>().sprite = rateSprites[Mathf.RoundToInt(HeartMachine.Instance.Vitality * (rateSprites.Count -1))];
+    }
+}
