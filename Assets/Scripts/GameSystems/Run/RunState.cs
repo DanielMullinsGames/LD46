9ca1d20665b618aa0e04e341a2796fc2e6e7520c
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2020-04-18 23:44:55 -0700

    - game over sequence completed + resetting

--- Assets/Scripts/GameSystems/Run/RunState.cs
+++ Assets/Scripts/GameSystems/Run/RunState.cs
@@ -7,4 +7,11 @@ public class RunState
     public static int bullets = 3;
     public static int coal = 21;
     public static bool lostHeart;
+
+    public static void Reset()
+    {
+        bullets = 3;
+        coal = 21;
+        lostHeart = false;
+    }
 }
---
+++ Assets/Scripts/Story/GameOverSequencer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverSequencer : MonoBehaviour
+{
+    [SerializeField]
+    private SequentialText text;
+
+    [SerializeField]
+    private GameObject choices;
+
+    private void Start()
+    {
+        StartCoroutine(GameOverSequence());
+    }
+
+    private IEnumerator GameOverSequence()
+    {
+        yield return new WaitForSeconds(4f);
+        text.PlayMessage("end of the line...    start over from seattle?");
+        yield return new WaitUntil(() => !text.PlayingMessage);
+        yield return new WaitForSeconds(0.2f);
+
+        choices.SetActive(true);
+        AudioController.Instance.PlaySound2D("crunch_blip");
+
+        yield return new WaitUntil(() => Input.GetButton("PlayerUp") || Input.GetButton("PlayerDown"));
+
+        if (Input.GetButton("PlayerUp"))
+        {
+            text.gameObject.SetActive(false);
+            choices.SetActive(false);
+            AudioController.Instance.PlaySound2D("crunch_blip");
+            RunState.Reset();
+            yield return new WaitForSeconds(0.5f);
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Scene2");
+        }
+        else
+        {
+            text.gameObject.SetActive(false);
+            choices.SetActive(false);
+            AudioController.Instance.PlaySound2D("crunch_blip");
+            yield return new WaitForSeconds(0.5f);
+            Application.Quit();
+        }
+    }
+}
