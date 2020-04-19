834a14c17be51bfb5fd72178c69bf3fb351538e7
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2020-04-19 15:52:06 -0700

    - start screen

---
+++ Assets/Scripts/Story/StartSequencer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSequencer : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(BeepingSlow());
+    }
+
+    private void Update()
+    {
+        if (Time.timeSinceLevelLoad < 1f)
+        {
+            return;
+        }
+
+        if (Input.GetButton("PlayerLeft") || Input.GetButton("PlayerRight") || Input.GetButton("PlayerUp") || Input.GetButton("PlayerDown"))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Scene1");
+        }
+    }
+
+    private IEnumerator BeepingSlow()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2f);
+            AudioController.Instance.PlaySound2D("short_beep", volume: 0.1f);
+        }
+    }
+}
