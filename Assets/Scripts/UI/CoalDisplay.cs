af6772a13a4d75be280486916f6a0dd6c7adf4a0
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2020-04-19 16:07:30 -0700

    - minor adjustments

--- Assets/Scripts/UI/CoalDisplay.cs
+++ Assets/Scripts/UI/CoalDisplay.cs
@@ -8,19 +8,19 @@ public class CoalDisplay : Singleton<CoalDisplay>
     [SerializeField]
     private TMPro.TextMeshProUGUI text;
 
-    private Vector3 basePos;
+    [SerializeField]
+    private Color textColor;
 
     private void Start()
     {
-        basePos = transform.localPosition;
-        UpdateDisplay();
+        UpdateDisplay(flicker: false);
     }
 
-    public void UpdateDisplay()
+    public void UpdateDisplay(bool flicker = true)
     {
         text.SetText("x" + RunState.coal.ToString());
 
-        Tween.LocalPosition(transform, basePos + (Vector3.up * 0.03f), 0.1f, 0f, Tween.EaseOut);
-        Tween.LocalPosition(transform, basePos, 0.1f, 0.15f, Tween.EaseIn);
+        text.color = default;
+        CustomCoroutine.WaitThenExecute(0.1f, () => text.color = textColor);
     }
 }
