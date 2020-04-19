919bee3ef98e6bc445698a1bf22696ae0e6e33fa
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2020-04-18 17:45:58 -0700

    - raider + game over progress

---
+++ Assets/Scripts/GameSystems/Events/Raider.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raider : MonoBehaviour
+{
+    public float timeToShoot = 3f;
+
+    private float shootTimer = 0f;
+    private bool canShoot;
+
+    public void OnBecomeShootableKeyframe()
+    {
+
+    }
+
+    public void GrappleKeyframe()
+    {
+        AudioController.Instance.PlaySound2D("crunch_short_2");
+    }
+
+    public void HandKeyframe()
+    {
+        AudioController.Instance.PlaySound2D("crunch_blip");
+    }
+
+    public void AimKeyframe()
+    {
+        AudioController.Instance.PlaySound2D("crunch_blip");
+    }
+
+    public void OnPlayerCanBeKilledKeyframe()
+    {
+        canShoot = true;
+    }
+
+    private void Update()
+    {
+        if (canShoot)
+        {
+            shootTimer += Time.deltaTime;
+            if (shootTimer > timeToShoot)
+            {
+                shootTimer = 0f;
+                Shoot();
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        var gunshot = AudioController.Instance.PlaySound2D("gunshot");
+        DontDestroyOnLoad(gunshot.gameObject);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Dialogue");
+    }
+}
