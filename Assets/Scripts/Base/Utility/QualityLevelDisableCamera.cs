c73760bd46069c1effe5bfcac602a6eeae27dbfd
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2020-05-16 19:27:12 -0700

    - Llow quality setting disables post processing camera

---
+++ Assets/Scripts/Base/Utility/QualityLevelDisableCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QualityLevelDisableCamera : MonoBehaviour
+{
+    public int disableIfLower;
+
+    private void Start()
+    {
+        if (QualitySettings.GetQualityLevel() <= disableIfLower)
+        {
+            GetComponent<Camera>().enabled = false;
+        }
+    }
+}
